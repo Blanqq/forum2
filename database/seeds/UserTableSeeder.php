0655ad05a3cff6421153bfcdc185b9802cb21cfe
Pawel <vanderuard@gmail.com>
2018-08-20 23:58:44 +0200

    Basic database seeding.

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'User';
+        $user->email = 'user@example.com';
+        $user->password = bcrypt('user');
+        $user->save();
+    }
+}
